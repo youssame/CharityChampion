9d4e757467933fad4cee10d9f8eccf602aa62502
Youssef Ameachaq <youssefameachaq@gmail.com>
2023-06-25 13:12:37 +0100

    Project setup

---
+++ src/main/java/com/charitychampion/charitychampion/CharityChampionApplication.java
@@ -0,0 +1,13 @@
+package com.charitychampion.charitychampion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityChampionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityChampionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charitychampion/charitychampion/CharityChampionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charitychampion.charitychampion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityChampionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
