c75bde9a0f936edd1e88c916bc8cf65170c0cc40
foudal <foudalayoub1998@gmail.com>
2023-09-18 15:21:27 +0100

    add class SpringSecurityConfig and LoginController edit on ProjectControllerTest class

--- src/test/java/com/charitychampion/charitychampion/project/controller/ProjectControllerTest.java
+++ src/test/java/com/charitychampion/charitychampion/project/controller/ProjectControllerTest.java
@@ -1,9 +1,16 @@
 package com.charitychampion.charitychampion.project.controller;
+import com.charitychampion.charitychampion.project.repositories.IProjectRepository;
 import com.charitychampion.charitychampion.project.services.ProjectService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.inject.Inject;
 import com.google.inject.matcher.Matchers;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 //import org.mockito.Mock;
 import org.mockito.Mock;
@@ -11,6 +18,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.http.HttpStatus;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -21,34 +29,70 @@ import com.charitychampion.charitychampion.project.enums.EProjectStatus;
 import com.charitychampion.charitychampion.project.enums.EProjectType;
 import com.charitychampion.charitychampion.project.model.Project;
 import com.charitychampion.charitychampion.project.requests.CreateProjectRequest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
+@WebMvcTest(ProjectController.class)
+//@ExtendWith(SpringExtension.class)
 public class ProjectControllerTest {
     @Autowired
-    private ProjectController projectController;
-    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
     private ProjectService projectService;
 
-    @Test
-    public void whenRequestValid_thenProjectCreated() {
-        Project returnProject = new Project();
-        returnProject.setTitle("This");
-        returnProject.setDescription("This");
-        returnProject.setType(EProjectType.OTHER);
-        returnProject.setStatus(EProjectStatus.OPEN);
-    Mockito.when(projectService.save(Mockito.any(Project.class))).thenReturn(returnProject);
-    CreateProjectRequest projectRequest = new CreateProjectRequest();
-    projectRequest.description = "This is the first project";
-    projectRequest.title = "Project 1";
-    projectRequest.status = EProjectStatus.OPEN;
-    projectRequest.type = EProjectType.OTHER;
-    projectRequest.neededAmount = 100L;
+    @MockBean
+    private IProjectRepository iProjectRepository;
 
-    ResponseEntity<Project> resultProject = projectController.create(projectRequest);
-    
-    Assert.assertEquals(resultProject.getStatusCode(), HttpStatusCode.valueOf(201));
+    @Before
+    public void setUp(){
+        iProjectRepository=mock(iProjectRepository.getClass());
+        projectService=new ProjectService(iProjectRepository);
+    }
+
+    @Test
+    public void whenRequestValid_thenProjectCreated() throws Exception {
+    //given
+        CreateProjectRequest projectRequest = new CreateProjectRequest();
+        projectRequest.description = "description";
+        projectRequest.title = "title";
+        projectRequest.status = EProjectStatus.OPEN;
+        projectRequest.type = EProjectType.POOR_FAMILY;
+        projectRequest.neededAmount = 100L;
+
+        Project project=new Project();
+        project.setDescription("description");
+        project.setStatus(EProjectStatus.OPEN);
+        project.setTitle("title");
+        project.setType(EProjectType.POOR_FAMILY);
+        project.setNeededAmount(100L);
+
+        when(projectService.save(project)).thenReturn(project);
+
+        Project response=this.projectService.save(project);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonPayload = objectMapper.writeValueAsString(response);
+        //System.out.printf("json"+jsonPayload);
+    //when
+        final MvcResult result =mockMvc.perform(MockMvcRequestBuilders.post("/api/projects")
+                        .contentType("application/json")
+                        .content("{\"description\":\"description\",\"status\":\"OPEN\",\"title\":\"title\",\"type\":\"POOR_FAMILY\",\"neededAmount\":\"100\"}"))
+                        //.content(objectMapper.writeValueAsString(project)))
+                //.content(jsonPayload))
+                        .andExpect(MockMvcResultMatchers.status().isCreated())
+                        .andReturn();
+    //then
+        //assertThat(result.getResponse().getContentAsString()).isEqualTo(jsonPayload);
+        //verify(this.projectService,times(1)).save(project);
    }
 
 }
